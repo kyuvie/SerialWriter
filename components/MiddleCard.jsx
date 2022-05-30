d8febd3b995959e553d4d2f61499b1a252c6dbc8
M0GW4I <starlight_express_77@hotmail.co.jp>
2022-05-30 10:53:32 +0900

    add middle card jsx

---
+++ components/MiddleCard.jsx
@@ -0,0 +1,21 @@
+function create_middle_card(obj) {
+    return (<div className="card">
+        <div className="card-body">
+            <h5 className="card-title">Command</h5>
+            <h6 className="card-subtitle mb-2 text-muted">command description</h6>
+            <div className="row align-items-center">
+                <div className="col-auto">
+                    <label htmlFor="inputPassword6" className="col-form-label">value</label>
+                </div>
+                <div className="col-auto">
+                    <input type="password" id="inputPassword6" className="form-control form-control-sm" aria-describedby="passwordHelpInline" />
+                </div>
+            </div>
+            <div className="row align-items-center justify-content-end pt-4">
+                <div className="col-auto">
+                    <button type="button" class="btn btn-primary btn-sm">add</button>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
